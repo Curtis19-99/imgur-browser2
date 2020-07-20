f9ae347f7878e84d337a9161db1f21b8e35cd2a0
Curtis Chavez <cchavez539@cnm.edu>
2020-07-20 16:58:11 -0600

    Added spinner view.

--- app/src/main/java/edu/cnm/deepdive/imgurbrowser2/view/GalleryListAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/imgurbrowser2/view/GalleryListAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -12,7 +13,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import edu.cnm.deepdive.imgurbrowser2.R;
 import edu.cnm.deepdive.imgurbrowser2.model.Gallery;
 
-public class GalleryListAdapter extends RecyclerView.Adapter<GalleryListAdapter.GalleryViewHolder> {
+public class GalleryListAdapter extends
+    RecyclerView.Adapter<GalleryListAdapter.GalleryViewHolder> {
 
   private final Context context;
   private final Gallery[] galleries;
@@ -27,7 +29,7 @@ public class GalleryListAdapter extends RecyclerView.Adapter<GalleryListAdapter.
   @Override
   public GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
       int viewType) {
-    View view = LayoutInflater.from(context).inflate(R.layout.item_gallery_search,parent,false);
+    View view = LayoutInflater.from(context).inflate(R.layout.item_gallery_search, parent, false);
     return new GalleryViewHolder(view);
   }
 
@@ -52,11 +54,25 @@ public class GalleryListAdapter extends RecyclerView.Adapter<GalleryListAdapter.
       title = itemView.findViewById(R.id.title);
       description = itemView.findViewById(R.id.description);
       imageSpinner = itemView.findViewById(R.id.gallery_search_spinner);
+      imageSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+//          String item = adapterView.getItemAtPosition(position).toString();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+      });
     }
 
     private void bind(int position) {
       title.setText(galleries[position].getTitle());
       description.setText(galleries[position].getDescription());
+      GalleryImageAdapter galleryImageAdapter = new GalleryImageAdapter(context,
+          galleries[position].getImages());
+      imageSpinner.setAdapter(galleryImageAdapter);
     }
   }
 
